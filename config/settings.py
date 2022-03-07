749e4258c4eee7ec5639d8b4acef73f6c824b8e3
Khasan <khasankamalov0911@gmail.com>
2022-03-07 10:45:01 +0500

    herokuga yuklash gunicorn

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-&6b-b@h=$$h26ssymwhvqy_lavnvnhn(@g-6-=8oh^x(!hwsac
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*',]
 
 
 # Application definition
