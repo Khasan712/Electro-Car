e4ea5dbd8c194c5798224a03819d92373f0f3140
Khasan <khasankamalov0911@gmail.com>
2022-03-10 12:51:17 +0500

    Car API funsiyasi o'chirildi

--- mainapp/router.py
+++
@@ -1,8 +0,0 @@
-from .serializers import CarSerializers
-from .views import CreateCarView
-from rest_framework import routers
-
-
-
-# router = routers.DefaultRouter()
-# router.register('viewset', CreateCarView, basename='car')
--- mainapp/urls.py
+++ mainapp/urls.py
@@ -3,24 +3,6 @@ from . import views
 # from .router import router
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('create/image/', views.create_image, name='create_image'),
-    path('get/image/<int:pk>/', views.get_image, name='get_image'),
-    path('delete/image/<int:pk>/', views.delete_image, name='delete_image'),
-
-
-    path('create/color/', views.create_color, name='create_color'),
-    path('get/color/<int:pk>/', views.get_color, name='get_color'),
-    path('delete/color/<int:pk>/', views.delete_color, name='delete_color'),
-
-
-    path('create/brend/', views.create_brend, name='create_brend'),
-    path('get/brend/<int:pk>/', views.get_brend, name='get_brend'),
-    path('delete/brend/<int:pk>/', views.delete_brend, name='delete_brend'),
-
-
     path('create/car/', views.CreateCarView.as_view(), name='create_car'),
     path('get/car/<int:pk>/', views.CarRetrieveUpdateDestroyAPIView.as_view(), name='get_car'),
-    path('delete/car/<int:pk>/', views.delete_car, name='delete_car'),
-    # path('create/car/view', include(router.urls))
 ]
